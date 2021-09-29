3fe87baa2bdfe629b1b35cc6107a97781fcb7bef
Andrei <karlikboroda@gmail.com>
2021-09-29 10:52:04 +0300

    Fix messages order issue and update one reducer function

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -18,7 +18,7 @@ export const addMessageToStore = (state, payload) => {
       newConvo.latestMessageText = message.text;
       return newConvo;
     } else {
-      return newConvo;
+      return convo;
     }
   });
 };
@@ -69,11 +69,12 @@ export const addSearchedUsersToStore = (state, users) => {
 
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
-    if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+    const newConvo = {...convo};
+    if (newConvo.otherUser.id === recipientId) {
+      newConvo.id = message.conversationId;
+      newConvo.messages.push(message);
+      newConvo.latestMessageText = message.text;
+      return newConvo;
     } else {
       return convo;
     }
